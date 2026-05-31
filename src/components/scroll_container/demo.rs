03e62d253426d1c9cd95c5c0865a9118ee31e300
Shannon Heylmun <sajshannon@gmail.com>
2026-05-31 12:29:38 -0400

    Remove boxed()

--- src/components/scroll_container/demo.rs
+++ src/components/scroll_container/demo.rs
@@ -126,7 +126,7 @@ impl<S: 'static> View<S, (), ViewCtx> for ScrollContainerDemoPanel {
 
     fn build(&self, ctx: &mut ViewCtx, _: &mut S) -> (Self::Element, Self::ViewState) {
         let mut local: DemoState = DemoState::default();
-        let view: InnerView = make_inner_view(&self.theme,local).boxed();
+        let view: InnerView = make_inner_view(&self.theme, local).boxed();
         let (element, view_state) = view.build(ctx, &mut local);
         (
             element,
@@ -146,7 +146,7 @@ impl<S: 'static> View<S, (), ViewCtx> for ScrollContainerDemoPanel {
         element: Mut<'_, Self::Element>,
         _: &mut S,
     ) {
-        let new_view: InnerView = make_inner_view(&self.theme,vs.local).boxed();
+        let new_view: InnerView = make_inner_view(&self.theme, vs.local).boxed();
         new_view.rebuild(&vs.view, &mut vs.view_state, ctx, element, &mut vs.local);
         vs.view = new_view;
     }
@@ -194,7 +194,7 @@ fn make_inner_view(theme: &Theme, vis: DemoState) -> impl WidgetView<DemoState> 
     .gap(Length::px(16.0));
 
     let scroll = sized_box(
-        scroll_container(content_grid::<DemoState>(theme, 12, 8))
+        scroll_container(content_grid::<DemoState>(theme, 12, 8).boxed())
             .constrain_horizontal(true)
             .scroll_bar_visibility(vis)
             .render(theme),
