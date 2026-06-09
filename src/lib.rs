670dbb17c56dae4008368be912b16cada8347bce
Shannon Heylmun <sajshannon@gmail.com>
2026-06-09 18:03:35 -0400

    Rebase cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -51,14 +51,9 @@ pub use components::{
     ScrollContainerView, SelectionState, Separator, SeparatorStyle, SidebarItem, SidebarItemView,
     SidebarPanel, SidebarPanelView, Slider, SliderView, SortDirection, SortState, Spinner,
     SpinnerView, SpinnerWidget, Tooltip, TooltipView, button, button_group, colored_text_column,
-    data_grid, filtered_indices, icon, label, optional_text_column, range_slider, read_only_text,
-    scroll_container, separator, sidebar_item, sidebar_panel, slider, spinner, text_column,
-    toggle_button_group, tooltip,
-    SidebarPanel, SidebarPanelView, Slider, SliderView, SortDirection, SortState, Tooltip,
-    TooltipView, button, button_group, colored_text_column, data_grid, dropdown_button,
-    filtered_indices, icon, label, optional_text_column, popover, range_slider, read_only_text,
-    scroll_container, separator, sidebar_item, sidebar_panel, slider, text_column,
-    toggle_button_group, tooltip,
+    data_grid, dropdown_button, filtered_indices, icon, label, optional_text_column, popover,
+    range_slider, read_only_text, scroll_container, separator, sidebar_item, sidebar_panel, slider,
+    spinner, text_column, toggle_button_group, tooltip,
 };
 pub use floating::{FloatingOverlay, FloatingOverlayView, floating, interactive_floating};
 #[cfg(feature = "gallery")]
