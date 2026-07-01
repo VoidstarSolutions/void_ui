69f81ae1c04828b99fdf918dc423f7970103b1ba
Shannon Heylmun <sajshannon@gmail.com>
2026-06-30 22:01:41 -0400

    Address code rabbit
    https://github.com/VoidstarSolutions/void_ui/pull/87#discussion_r3502178821

--- src/components/scroll_container/widget.rs
+++ src/components/scroll_container/widget.rs
@@ -543,6 +543,16 @@ impl<W: Widget + ?Sized> ScrollView<W> {
         if this.widget.viewport_pos != Point::ORIGIN {
             this.widget.viewport_pos = Point::ORIGIN;
             this.ctx.request_compose();
+            {
+                let (sb, mut sb_ctx) = this.ctx.get_raw_mut(&mut this.widget.scrollbar_h);
+                sb.cursor_progress = 0.0;
+                sb_ctx.request_render();
+            }
+            {
+                let (sb, mut sb_ctx) = this.ctx.get_raw_mut(&mut this.widget.scrollbar_v);
+                sb.cursor_progress = 0.0;
+                sb_ctx.request_render();
+            }
         }
     }
 }
